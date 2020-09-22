389fc5bc5ef7003a3ec2495b7681a4127768c498
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-22 13:54:00 +0300

    Connecting to MongoDB

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
 
 const app = express()
+
+mongoose.connect(keys.mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance')
+})
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err)
+})
 
 app.get('/', (req, res) => {
   res.send('Hi there')
