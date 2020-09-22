196f1552a6498f1d3e5cb3e9836a723709665c35
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-22 19:13:15 +0300

    Fix typo

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -31,7 +31,7 @@ router.post('/signin', async (req, res) => {
   const { email, password } = req.body
 
   if (!email || !password) {
-    return res.status(422).send({ error: 'Must provide eamil and password' })
+    return res.status(422).send({ error: 'Must provide email and password' })
   }
 
   const user = await User.findOne({ email })
