e8cbe5e0807444a3316ff28aabe9dab5e5d59e19
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-22 19:02:33 +0300

    Fix typo and missing argument

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -13,13 +13,13 @@ const userSchema = new mongoose.Schema({
   },
 })
 
-userSchema.pre('save', function () {
+userSchema.pre('save', function (next) {
   const user = this
   if (!user.isModified('password')) {
     return next()
   }
 
-  bcrypt.getSalt(10, (err, salt) => {
+  bcrypt.genSalt(10, (err, salt) => {
     if (err) {
       return next(err)
     }
