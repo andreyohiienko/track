bbb066e7378b6afe45fc8558a3f9d056f157bbd9
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-23 14:25:30 +0300

    What's the context doing

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -10,8 +10,31 @@ const authReducer = (state, action) => {
   }
 }
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+    // if we sign up, modify our state, and say that we are authencticated
+    // if signing up fails, we probably need to reflect an error message
+    // somewhere
+  }
+}
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating satte
+    // handle failure by showing errror message (somehow)
+  }
+}
+
+const signout = (dispatch) => {
+  return ({ email, password }) => {
+    // somehow sign out!!
+  }
+}
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedin: false },
 )
