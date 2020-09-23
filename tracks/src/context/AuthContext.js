a426b9c33c07b08ecc45b3a6f489a91c499387fa
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-23 14:51:53 +0300

    Making an API request

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext'
+import trackerApi from '../api/tracker'
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -11,8 +12,14 @@ const authReducer = (state, action) => {
 }
 
 const signup = (dispatch) => {
-  return ({ email, password }) => {
+  return async ({ email, password }) => {
     // make api request to sign up with that email and password
+    try {
+      const response = await trackerApi.post('/signup', { email, password })
+      console.log(response.data)
+    } catch (error) {
+      console.log(error.response.data)
+    }
     // if we sign up, modify our state, and say that we are authencticated
     // if signing up fails, we probably need to reflect an error message
     // somewhere
@@ -22,7 +29,7 @@ const signup = (dispatch) => {
 const signin = (dispatch) => {
   return ({ email, password }) => {
     // Try to signin
-    // Handle success by updating satte
+    // Handle success by updating state
     // handle failure by showing errror message (somehow)
   }
 }
