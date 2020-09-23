d932f1be0d719ddd42ec1491f4ecd2ab8d455718
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-23 13:58:46 +0300

    Input props

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Button, Text, Input } from 'react-native-elements'
 import Spacer from '../components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign up for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
       <Spacer />
-      <Input label="Password" />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+      />
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
