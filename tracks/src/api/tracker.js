daa9ae86ec46514a6d944096dfa75580ce122173
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-25 13:13:32 +0300

    Navigation from outside of react

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://51f7cbbc393c.ngrok.io',
+  baseURL: 'http://4605f7b85ea4.ngrok.io',
 })
--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import { AsyncStorage } from 'react-native'
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
+import { navigate } from '../navigationRef'
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -20,9 +21,13 @@ const signup = (dispatch) => async ({ email, password }) => {
     const response = await trackerApi.post('/signup', { email, password })
     await AsyncStorage.setItem('token', response.data.token)
     dispatch({ type: 'signup', payload: response.data.token })
+
+    // navigate to main flow
+    navigate('TrackList')
   } catch (error) {
     // if signing up fails, we probably need to reflect an error message
     // somewhere
+    console.log(error)
     dispatch({
       type: 'add_error',
       payload: 'Something went wrong with sign up.',
