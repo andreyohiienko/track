253571f4f1e498dbeb8ccb60effb15377f051adc
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-25 13:53:05 +0300

    Real component reuse

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,18 +1,37 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
 
 const SigninScreen = () => {
   return (
-    <View>
-      <Text style={styles.title}>SigninScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to your account"
+        errorMessage=""
+        onSubmit={() => {}}
+        submitButtonText="Sign In"
+      />
+      <NavLink
+        text="Dont have an account? Sign up instead"
+        routeName="Signup"
+      />
     </View>
   )
+}
+
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  }
 }
 
 export default SigninScreen
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 48,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
   },
 })
