2882b87fc4314f9f0dacc7258c2bbc57d33fd094
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-25 15:04:58 +0300

    Showing a Map

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import MapView from 'react-native-maps'
 
 const Map = () => {
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <MapView
+      initialRegion={{
+        latitude: 37.33233,
+        longitude: -122.03121,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+      style={styles.map}
+    />
   )
 }
 
 export default Map
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+})
