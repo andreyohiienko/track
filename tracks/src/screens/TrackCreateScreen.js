cc8a1cd5cedf9da0969fb02cfcd7accb3aae5bca
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-09-26 22:45:07 +0300

    Reading a location

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,5 +1,9 @@
 import '../_mockLocation'
-import { requestPermissionsAsync } from 'expo-location'
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from 'expo-location'
 import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
@@ -12,6 +16,16 @@ const TrackCreateScreen = () => {
   const startWatching = async () => {
     try {
       await requestPermissionsAsync()
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          console.log(location)
+        },
+      )
     } catch (error) {
       setErr(error)
     }
